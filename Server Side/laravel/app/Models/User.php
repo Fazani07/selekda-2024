f27974229206c95f27fe82d4266111b1c993bc47
fazaradityarma@gmail.com <fazaradityarma@gmail.com>
2024-08-29 09:11:22 +0700

    membuat models dan migration

--- Server Side/laravel/app/Models/User.php
+++ Server Side/laravel/app/Models/User.php
@@ -6,6 +6,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
 
 class User extends Authenticatable
 {
@@ -20,6 +22,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'date_of_birth', 
+        'phone_number', 
+        'profile_picture',
     ];
 
     /**
@@ -28,7 +33,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
+        'password', 
         'remember_token',
     ];
 
@@ -41,7 +46,6 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
         ];
     }
 }
